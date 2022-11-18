98bb8b5ef44f0dc0c9d4a869e5791a20d0d4bba7
Дарья Чернова <dariashvartz@mail.ru>
2022-11-18 21:09:35 +0300

    Group and aggregate corruption level data, visualize it with plot function

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     df = pd.read_csv('2019.csv')
-    print(df.columns)
-    df = df.rename(columns = {'Overall rank' : 'overall_rank', 'Country or region': 'region', 'GDP per capita' : 'gdp'})
-    print(df.columns)
+    print(df.describe())
+    df = df.rename(columns = {'Overall rank' : 'overallRank',
+                              'Country or region': 'region',
+                              'Score' : 'score',
+                              'GDP per capita' : 'gdp',
+                              'Social support' : 'socialSupport',
+                              'Healthy life expectancy' : 'healthyLifeExpectancy',
+                              'Freedom to make life choices' : 'freedomForChoices',
+                              'Generosity' : 'generosity',
+                              'Perceptions of corruption' : 'corruptionLevel'})
 
+    corruptionLevel = df \
+        .groupby(['region', 'score'], as_index=False) \
+        .aggregate({'corruptionLevel': 'sum'}) \
+        .sort_values('corruptionLevel', ascending=False)
+
+    corruptionLevel.plot(y='corruptionLevel', x='region')
+    plt.show()
